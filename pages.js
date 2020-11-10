80b0c0385d1abdda20d029433d8ad9b95a7dde6d
Vethursan Jegatheeswaran <j.vethursan@gmail.com>
2020-11-09 21:26:55 -0500

    v3 of site

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/vethursan/vethursan.github.io.git";
+const repoURL = "https://github.com/Vethursan/vethursan.github.io.git";
 
 ghpages.publish(
   pathname,
